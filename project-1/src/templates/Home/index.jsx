00980e6fac0424f0a45329fe53126a98da52b3c1
Gabriel Lira <bieeww73@gmail.com>
2023-04-21 14:51:44 -0300

    inserindo input

--- project-1/src/templates/Home/index.jsx
+++ project-1/src/templates/Home/index.jsx
@@ -13,25 +13,8 @@ export default class Home extends Component {
     allPosts: [],
     page: 0,
     postsPerPage: 10,
+    searchValue: "",
   };
-
-  /* Lifecycle Methods
-     Montagem
-    - constructor()
-    - getDerivedStateFromProps()
-    - render()
-    - componentDidMount()
-
-     Atualização
-    - getDerivedStateFromProps()
-    - shouldComponentUpdate()
-    - render()
-    - getSnapshotBeforeUpdate()
-    - componentDidUpdate()
-    
-     Desmontagem
-    - componentWillUnmount()
-  */
 
   // método não utiliza arrow function
 
@@ -50,39 +33,63 @@ export default class Home extends Component {
   };
 
   loadMorePosts = () => {
-    const {
-      page,
-      postsPerPage,
-      allPosts,
-      posts
-    } = this.state;
+    const { page, postsPerPage, allPosts, posts } = this.state;
 
     const nextPage = page + postsPerPage;
-    
+
     const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
 
     posts.push(...nextPosts);
 
-    this.setState({ posts, page: nextPage })
+    this.setState({ posts, page: nextPage });
   };
-  
+
+  handleChange = (e) => {
+    const { value } = e.target;
+    this.setState({ searchValue: value });
+  };
 
   render() {
-    const { posts, page, postsPerPage, allPosts } = this.state;
+    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
     const noMorePosts = page + postsPerPage >= allPosts.length;
 
     return (
       <section className="container">
+        {!!searchValue && (
+          <>
+            <h1>Search Value: {searchValue}</h1>
+          </>
+        )}
+
+        <input type="search" onChange={this.handleChange} value={searchValue} />
         <Posts posts={posts} />
         <div className="button_container">
-        <Button 
-          text="Load More Posts"
-          /* Passando apenas valor do onClick em props */
-          onClick={this.loadMorePosts}
-          disabled={noMorePosts}
-        />
+          <Button
+            text="Load More Posts"
+            /* Passando apenas valor do onClick em props */
+            onClick={this.loadMorePosts}
+            disabled={noMorePosts}
+          />
         </div>
       </section>
     );
   }
 }
+
+/* Lifecycle Methods
+     Montagem
+    - constructor()
+    - getDerivedStateFromProps()
+    - render()
+    - componentDidMount()
+
+     Atualização
+    - getDerivedStateFromProps()
+    - shouldComponentUpdate()
+    - render()
+    - getSnapshotBeforeUpdate()
+    - componentDidUpdate()
+    
+     Desmontagem
+    - componentWillUnmount()
+  */
